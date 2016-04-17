e31420e67b0ec51543fa6e1b073b131f95a2ade5
Renan Aragao Costa <aragaore@gmail.com>
2016-04-17 21:52:13 +0200

    Connect

--- bootstrap/connect.php
+++ bootstrap/connect.php
@@ -1,13 +1,13 @@
 <?php
 $servername = "localhost";
-						$username = "root";
-						$password = "";
-						$database = "example_database";
+$username = "root";
+$password = "";
+$database = "example_database";
 
-						// Create connection
-						$conn = new mysqli($servername, $username, $password, $database);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
 
-						// Check connection
-						if ($conn->connect_error) {
-							die("Connection failed: " . $conn->connect_error);
-						}
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
